06b704e2a65194dd79592705ee080c5db3c723df
halvardssm <halvard@simplyundoable.com>
2020-06-27 17:29:07 +0200

    added reusable core methods

--- src/utils.ts
+++ src/utils.ts
@@ -1,17 +1,54 @@
-import { WeaponStats } from 'TypeUtils'
+import { WeaponStats, Budget } from 'TypeUtils'
+import db from './assets/db.json';
 
-const absToNextLevel = (base: number, level = 0) => base + (level ? ((level - 1) * base / 2) : 0)
+export const STATS_ARRAY = ["attack", "flame", "chill", "lightning", "cyclone", "smash", "exorcism", "beast", "scale"]
 
-const absToLevel = (base: number, level = 0) => base + ((level * (level + 1)) / 2 - 1) * base / 2
+export const absToNextLevel = (base: number, level = 0) => base + (level ? ((level - 1) * base / 2) : 0)
 
-const statsAfterSpectrumizing = (stats: WeaponStats, targetAttackStat?: number) => Object.keys(stats).map(key => {
-  if (key === "attack") {
-    if (targetAttackStat && targetAttackStat >= stats[key]) {
-      stats[key] = Math.floor((0.25 * stats[key]) + (0.75 * (stats[key] - targetAttackStat)))
+export const absToLevel = (base: number, level = 0) => base + ((level * (level + 1)) / 2 - 1) * base / 2
+export const absBetweenLevels = (base: number, currentLevel = 0, targetLevel = 0) => absToLevel(base, targetLevel) - absToLevel(base, currentLevel)
+
+export const statsAfterSpectrumizing = (stats: WeaponStats, targetAttackStat: number) => {
+  const result = {}
+  STATS_ARRAY.map(key => {
+    if (key === "attack") {
+      if (targetAttackStat && targetAttackStat >= stats[key]) {
+        result[key] = Math.floor((0.25 * stats[key]) + (0.75 * (stats[key] - targetAttackStat)))
+      } else {
+        result[key] = Math.floor(0.25 * stats[key])
+      }
     } else {
-      stats[key] = Math.floor(0.25 * stats[key])
+      result[key] = Math.floor(stats[key] * 0.6)
     }
-  } else {
-    stats[key] = Math.floor(stats[key] * 0.6)
-  }
-})
+  })
+  return result
+}
+
+export const minStatsForBuildUp = (targetWeapon: WeaponStats) => {
+  const result: Record<string, any> = {}
+  STATS_ARRAY.map((key: any) => result[key] = targetWeapon[key] - Math.floor(0.1 * targetWeapon[key]))
+  return result
+}
+
+export const synthToStats = (stats: WeaponStats, targetStats: WeaponStats, budget: Budget = 'gilda') => {
+  const shopingList = []
+  let totalAbs = 0
+  STATS_ARRAY.forEach(stat => {
+    const statPoints = targetStats[stat] - stats[stat]
+    if (statPoints > 0) {
+      const spectrumizeIndex = budget === "abs" ? db.spectrumize[stat].length - 1 : 0
+      const item = db.items.find(el => el.id === db.spectrumize[stat][spectrumizeIndex])
+      console.log({ stat, item, val: item.specs[stat] })
+      const quantity = item.specs[stat] !== 0 ? Math.ceil(statPoints / item.specs[stat]) : 0
+      const costGilda = quantity * item.price
+      totalAbs += quantity
+      shopingList.push({
+        item,
+        quantity,
+        costGilda
+      })
+    }
+  })
+
+  return { totalAbs, shopingList }
+}

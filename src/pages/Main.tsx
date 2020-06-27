4f7699c889c71ed627f90a53a42c9b64ea37786c
halvardssm <halvard@simplyundoable.com>
2020-06-27 17:29:25 +0200

    updated main page

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,10 +1,48 @@
 import { AppBar, Button, Container, Grid, Toolbar, Typography, Card, CardMedia, CardContent, CardActions, Checkbox } from '@material-ui/core';
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import db from '../../db.json';
+import db from '../assets/db.json';
+import { synthToStats, minStatsForBuildUp } from '../utils'
+
 export const Main = () => {
   const { t } = useTranslation()
   const [selected, setSelected] = useState({})
+
+  // const stats = synthToStats({
+  //   "attack": 44,
+  //   "flame": 45,
+  //   "chill": 0,
+  //   "lightning": 0,
+  //   "cyclone": 45,
+  //   "smash": 45,
+  //   "exorcism": 0,
+  //   "beast": 30,
+  //   "scale": 0,
+  // }, {
+  //   "attack": 60,
+  //   "flame": 0,
+  //   "chill": 0,
+  //   "lightning": 0,
+  //   "cyclone": 75,
+  //   "smash": 60,
+  //   "exorcism": 0,
+  //   "beast": 60,
+  //   "scale": 45,
+  // })
+
+  const stats = minStatsForBuildUp({
+    "attack": 55,
+    "flame": 60,
+    "chill": 0,
+    "lightning": 0,
+    "cyclone": 0,
+    "smash": 0,
+    "exorcism": 0,
+    "beast": 0,
+    "scale": 0,
+  })
+  console.log(stats)
+
   return (
     <Container maxWidth="sm">
       <Button size="small">
@@ -12,7 +50,7 @@ export const Main = () => {
       </Button>
       <Grid container spacing={2} justify="center">
 
-        {Object.keys(db.weapons).map((el, i) => {
+        {Object.keys(db.guides).map((el, i) => {
           const obj = db.weapons[el]
 
           return (<Grid key={i} item xs={12} sm={6} md={4} >
